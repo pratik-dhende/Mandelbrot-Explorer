566119a6b0f4d5382ad2d3512123962b50757f1e
pratik-dhende <pratikdhende2000@gmail.com>
2020-05-19 04:28:06 +0530

    First commit

---
+++ Assets/Mandelbrot Set.shader
@@ -0,0 +1,98 @@
+﻿Shader "Explorer/Mandelbrot Set"
+{
+    Properties
+    {
+		_MainTex("Texture", 2D) = "white" {}
+		_MaxIterations("MaxIterations", int) = 255
+		_Area("Area", vector) = (0, 0, 4, 4)
+		_Angle("Angle", range(-3.1415, 3.1415)) = 0.0
+		_Gradientcolor("Gradient Color", Color) = (0.3, 0.45, 0.65, 1)
+		_GradientRange("Gradient Range", float) = 20.0
+		[Toggle(USE_TEXTURE)] _OriginalMandelbrot("Original Mandelbrot", int) = 0
+		[Toggle(USE_TEXTURE)] _FixMandelbrotColor("Fix Mandelbrot", int) = 0
+		_MandelbrotColor("Mandelbrot Color", Color) = (0.0, 0.0, 0.0, 1)
+		_OuterColor("Outer Color", Color) = (1.0, 1.0, 1.0, 1)
+    }
+    SubShader
+    {
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+
+            sampler2D _MainTex;
+			int _MaxIterations;
+			float4 _Area;
+			float _Angle;
+			float4 _Gradientcolor;
+			float _GradientRange;
+			int _FixMandelbrotColor;
+			float4 _MandelbrotColor;
+			int _OriginalMandelbrot;
+			float4 _OuterColor;
+
+			float2 rotate(float2 uv, float2 pivot, float angle) {
+				uv -= pivot;
+				return float2((uv.x * cos(angle) - uv.y * sin(angle)), (uv.x * sin(angle) + uv.y * cos(angle))) + pivot;
+			}
+
+			float4 colorFunction(float col) {
+				float4 colorGradient = sin(_Gradientcolor * col * _GradientRange) * 0.5f + 0.5f;
+
+				return colorGradient;
+			}
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+				float2 c = _Area.xy + (i.uv - 0.5) * _Area.zw;
+				c = rotate(c, _Area.xy, _Angle);
+				//c = rotate(c, float2(0.0f, 0.0f), _Angle);
+				float2 z;
+
+				int iterations;
+
+				while (length(z) < 4 && iterations < _MaxIterations) {
+					z = float2((z.x * z.x) - (z.y * z.y), 2 * z.x * z.y) + c;
+					iterations += 1;
+				}
+
+				float4 col = sqrt(float(iterations) / float(_MaxIterations));
+
+				if (iterations >= _MaxIterations && _FixMandelbrotColor)
+						return _MandelbrotColor;
+
+				if (_OriginalMandelbrot)
+					return _OuterColor * col;
+
+				return colorFunction(col);
+            }
+            ENDCG
+        }
+    }
+}
---
+++ Assets/MandelbrotExplorer.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MandelbrotExplorer : MonoBehaviour
+{
+    public Material mat;
+
+    public Vector2 position;
+
+    public float scale = 4;
+    public float moveSpeed = 2f;
+    public float angle = 0f;
+    public float smoothFactor = 0.03f;
+    public float scrollSpeed = 0.1f;
+    public float gradientRange = 20.0f;
+    public int maxIterations = 255;
+
+    public bool fixMandelbrotColor = false;
+    public bool originalMandelbrotColor = false;
+    public Color mandelbrotGradient = Color.white;
+    public Color mandelbrotColor = Color.black;
+    public Color outerColor = Color.white;
+
+
+    Vector2 smoothPosition;
+    float smoothScale;
+    float smoothAngle;
+
+    bool mousePressed = false;
+
+    void UpdateMandelbrot()
+    {
+        smoothPosition = Vector2.Lerp(smoothPosition, position, smoothFactor);
+        smoothScale = Mathf.Lerp(smoothScale, scale, smoothFactor);
+        smoothAngle = Mathf.Lerp(smoothAngle, angle, smoothFactor);
+
+        float scaleX = smoothScale;
+        float scaleY = smoothScale;
+
+        float aspectRatio = (float)Screen.width / (float)Screen.height;
+
+        if (aspectRatio >= 1)
+        {
+            scaleY /= aspectRatio;
+        }
+        else
+        {
+            scaleX *= aspectRatio;
+        }
+        
+        mat.SetFloat("_MaxIterations", maxIterations);
+        mat.SetVector("_Area", new Vector4((!mousePressed) ? smoothPosition.x : position.x, (!mousePressed) ? smoothPosition.y : position.y, scaleX, scaleY));
+        mat.SetFloat("_Angle", smoothAngle);
+        mat.SetFloat("_GradientRange", gradientRange);
+        mat.SetColor("_Gradientcolor", mandelbrotGradient);
+
+        if (originalMandelbrotColor)
+        {
+            mat.SetInt("_OriginalMandelbrot", 1);
+            mat.SetColor("_OuterColor", outerColor);
+        }
+        else
+        {
+            mat.SetInt("_OriginalMandelbrot", 0);
+        }
+        if (fixMandelbrotColor)
+        {
+            mat.SetInt("_FixMandelbrotColor", 1);
+            mat.SetColor("_MandelbrotColor", mandelbrotColor);
+        }
+        else
+        {
+            mat.SetInt("_FixMandelbrotColor", 0);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        HandleInputs();
+        UpdateMandelbrot();   
+    }
+
+    void HandleInputs()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector2 xDir = Vector2.right;
+        Vector2 yDir = Vector2.up;
+
+        float cosa = Mathf.Cos(angle);
+        float sina = Mathf.Sin(angle);
+
+        xDir = new Vector2(xDir.x * cosa - xDir.y * sina, xDir.x * sina + xDir.y * cosa);
+        yDir = new Vector2(-xDir.y, xDir.x);
+
+        if (!mousePressed)
+        {
+            position += xDir * (horizontal * moveSpeed * scale);
+            position += yDir * (vertical * moveSpeed * scale);
+        }
+
+        //if (!mousePressed)
+            //position += new Vector2(horizontal, vertical) * moveSpeed * scale;
+
+        if (Input.GetMouseButton(0))
+        {
+            float mouseX = Input.GetAxisRaw("Mouse X");
+            float mouseY = Input.GetAxisRaw("Mouse Y");
+
+            Debug.Log(mouseX + " " + mouseY);
+
+            //position -= new Vector2(mouseX, mouseY) * moveSpeed * scale;
+            position -= xDir * (mouseX * moveSpeed * scale);
+            position -= yDir * (mouseY * moveSpeed * scale);
+
+            smoothPosition = position;
+            mousePressed = true;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            mousePressed = false;
+        }
+
+        float mouseScroll = Input.mouseScrollDelta.y;
+
+        if (mouseScroll > 0)
+        {
+            scale *= 0.99f - scrollSpeed;
+        }
+        else if (mouseScroll < 0)
+        {
+            scale *= 1.01f + scrollSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.KeypadPlus))
+        {
+            scale *= 0.99f;
+        }
+        else if (Input.GetKey(KeyCode.KeypadMinus))
+        {
+            scale *= 1.01f;
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            angle += 0.01f;
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            angle -= 0.01f;
+        }
+    }
+}
